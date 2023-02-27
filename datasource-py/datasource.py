cadf2d209cae8228220779f8a2bf229fc57b262a
haertst <s.haertel@itsonix.eu>
2023-02-27 15:01:18 +0100

    Try docker secrets

--- datasource-py/datasource.py
+++ datasource-py/datasource.py
@@ -1,13 +1,21 @@
 # stream_generator
+import praw
+import time
 print("Hello world!")
 
-import praw
+secrets = open("/run/secrets/frankenstein-secrets", "r")
+print(secrets.read())
 
-reddit = praw.Reddit(
-    client_id="foo",
-    client_secret="bar",
-    user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
-)
 
-for submission in reddit.subreddit("askreddit").new(limit=3):
-    print(submission.title)
+# while(True):
+#     time.sleep(3)
+#     print("foo")
+
+# reddit = praw.Reddit(
+#     client_id="foo",
+#     client_secret="bar",
+#     user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+# )
+
+# for submission in reddit.subreddit("askreddit").new(limit=3):
+#     print(submission.title)
