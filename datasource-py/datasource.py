d7e6528a760143d1abf21162d3caf51980c9d2df
Stefan Härtel <haertel.dev@gmail.com>
2023-02-27 22:33:57 +0100

    Reddit api works

--- datasource-py/datasource.py
+++ datasource-py/datasource.py
@@ -1,15 +1,16 @@
 # stream_generator
 import praw
-import time
+import os
+
 print("Hello world!")
 
-secrets = open("/run/secrets/frankenstein-secrets", "r")
-print(secrets.read())
+client_id_value = open(os.environ["CLIENT_ID_FILE"]).readline().rstrip()
+client_secret_value = open(os.environ["CLIENT_SECRET_FILE"]).readline().rstrip()
 
 reddit = praw.Reddit(
-    client_id="foo",
-    client_secret="bar",
-    user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+    client_id = client_id_value,
+    client_secret = client_secret_value,
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
 )
 
 for submission in reddit.subreddit("askreddit").new(limit=3):
