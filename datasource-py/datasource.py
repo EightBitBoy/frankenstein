249528c4ef27b20800a18e263c8b24fa0303c10f
haertst <s.haertel@itsonix.eu>
2023-02-28 08:10:21 +0100

    Use logging for printing out messages

--- datasource-py/datasource.py
+++ datasource-py/datasource.py
@@ -1,8 +1,27 @@
 # stream_generator
+import logging
 import praw
 import os
+import sys
 
 print("Hello world!")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
 
 client_id_value = open(os.environ["CLIENT_ID_FILE"]).readline().rstrip()
 client_secret_value = open(os.environ["CLIENT_SECRET_FILE"]).readline().rstrip()
@@ -13,5 +32,9 @@ reddit = praw.Reddit(
     user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
 )
 
-for submission in reddit.subreddit("askreddit").new(limit=3):
-    print(submission.title)
+# for submission in reddit.subreddit("askreddit").new(limit=3):
+#     print(submission.title)
+
+
+for comment in reddit.subreddit("askreddit").stream.comments():
+    logging.info(comment.author)
