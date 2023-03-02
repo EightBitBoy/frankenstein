f7930b4d5b2b802f0114352fa93816c945c3ab48
Stefan Härtel <haertel.dev@gmail.com>
2023-03-02 21:35:52 +0100

    Save progress

--- nlp/textprocessor.py
+++ nlp/textprocessor.py
@@ -9,6 +9,6 @@ print("Hello world!")
 import spacy
 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+doc = nlp("My beautiful wife is working at the computer.")
 for token in doc:
   print(token.text, token.pos_, token.dep_)
